308fab6efe4a94a827bbdd6ff04bffe61450bf63
Albert <albert@Alberts-MacBook-Pro.local>
2022-06-10 21:12:19 -0400

    next working properly with expo libraries

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -9,9 +9,12 @@ const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')([
   'solito',
   'app',
+  '@expo/vector-icons',
 ]);
 
+const withFonts = require('next-fonts');
+
 module.exports = withPlugins(
-  [withTM, [withExpo, { projectRoot: __dirname }]],
+  [withTM, withFonts, [withExpo, { projectRoot: __dirname }]],
   nextConfig,
 );
--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { GetStaticProps } from 'next';
 
 import { PostsDocument, PostsQuery, PostsQueryVariables } from 'app/graphql/generated/generated';
